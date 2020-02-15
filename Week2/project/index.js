5ac4d88f1c6836f2f09416a8c0587e41109308bd
hanbingli <57145537+hanbingli@users.noreply.github.com>
2020-02-15 23:33:24 +0100

    revised Week2 homework

--- Week2/project/index.js
+++ Week2/project/index.js
@@ -1,4 +1,11 @@
+
 'use strict';
+
+
+
+
+
+
 
 
 
@@ -11,7 +18,7 @@
     xhr.onload = () => {
       if (xhr.status >= 200 && xhr.status <= 299) {
         cb(null, xhr.response);
-        console.log(xhr.response)
+
       } else {
         cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
       }
@@ -23,13 +30,13 @@
 
 
 
-//*********** 创建并粘贴内容
+  //*********** 创建并粘贴内容
   function createAndAppend(name, parent, options = {}) {
     const elem = document.createElement(name);
     parent.appendChild(elem);
 
-  
-// *****************************************************************
+
+    // *****************************************************************
     Object.entries(options).forEach(([key, value]) => {
       if (key === 'text') {
 
@@ -46,30 +53,27 @@
 
 
   function renderRepoDetails(repo, ul) {
-  
-    
- 
-      const li =createAndAppend('li', ul);
-      li.setAttribute('class', 'li');
-   
-      const ul2 = createAndAppend('ul', li);
-      ul2.setAttribute('class', 'ul2');
-
-      const li1 =createAndAppend('a', ul2, { text: 'Repository: ' + repo.name });
-      li1.setAttribute('href', repo.html_url);
-
-      
 
 
+    const li = createAndAppend('li', ul);
+    li.setAttribute('class', 'li');
 
-      const li2 =createAndAppend('li', ul2, { text: 'Description: ' + repo.description });
-      const li3 =createAndAppend('li', ul2, { text: 'Forks: ' + repo.fork });
-      const li4 =createAndAppend('li', ul2, { text: 'Updated : ' + repo.updated_at });
- 
-      
+    const ul2 = createAndAppend('ul', li);
+    ul2.setAttribute('class', 'ul2');
+
+    const li1 = createAndAppend('a', ul2, { text: 'Repository: ' + repo.name });
+    li1.setAttribute('href', repo.html_url);
+    li1.setAttribute('class', 'repoName');
 
 
-  
+    const li2 = createAndAppend('li', ul2, { text: 'Description: ' + repo.description });
+    const li3 = createAndAppend('li', ul2, { text: 'Forks: ' + repo.fork });
+    const li4 = createAndAppend('li', ul2, { text: 'Updated : ' + repo.updated_at });
+
+
+
+
+
     // { text: repo.name }
 
   }
@@ -87,18 +91,22 @@
       const ul = createAndAppend('ul', root);
       ul.setAttribute('class', 'ul')
 
-      
-      
-      for(let  i = 0; i < 10; i++) {
-        renderRepoDetails(repos[i], ul);
-        let op =document.createElement('option');
-        op.text = repos[i].name ;
+      let dataRes = repos;
+      let dataSorted = dataRes.sort(function (a, b) {
+        return a.name.localeCompare(b.name)
+      });
+      console.log(dataSorted);
+
+      for (let i = 0; i < 10; i++) {
+        renderRepoDetails(dataSorted[i], ul);
+        let op = document.createElement('option');
+        op.text = dataSorted[i].name;
         dropdownL.appendChild(op);
-        
+
       }
 
-      
-     
+
+
     });
   }
 
@@ -125,9 +133,6 @@
       });
     }
   }
-  
-
-
 
 
 
@@ -135,24 +140,120 @@
   const HYF_REPOS_URL =
     'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
   window.onload = () => main(HYF_REPOS_URL);
+
+
+
+
+
+
+
+
+  const root = document.querySelector('#root');
+
+
+
+  const mainUl = document.querySelector('.ul');
+  const header = document.createElement('div');
+  header.setAttribute('class', 'header');
+  header.innerText = 'HYF Repositories';
+  const dropdownL = document.createElement('select');
+  header.appendChild(dropdownL);
+  dropdownL.setAttribute('class', 'dropdownL');
+  
+  document.body.insertBefore(header, root);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  dropdownL.addEventListener('change', function () {
+  
+  
+    let index = dropdownL.selectedIndex;
+    let selected = dropdownL.options[index].text;
+    console.log(selected);
+    let repoName = document.getElementsByClassName('repoName');
+
+    let ul = document.querySelector('.ul');
+    console.log(ul)
+    ul.setAttribute('class', 'ul_onclick')
+
+  
+    const contributerBox = document.createElement('div');
+    contributerBox.setAttribute('class', 'contributerBox');
+    root.appendChild(contributerBox);
+    contributerBox.innerHTML = 'contributions'
+  
+    const contriUl = document.createElement('ul');
+    contriUl.setAttribute('class', 'contriUl');
+    contributerBox.appendChild(contriUl);
+
+  
+  
+  
+    for (let i = 0; i < 10; ++i) {
+  
+      console.log(repoName[i].innerText)
+      if (repoName[i].innerText == 'Repository: ' + selected) {
+        let repoLi = repoName[i].parentElement.parentElement
+        console.log(true);
+        console.log(repoLi)
+  
+      } else {
+        repoName[i].parentElement.parentElement.style.display = 'none';
+      }
+  
+    }
+    contributerBox.style.display = 'block';
+
+  
+  
+  
+    // function appendContributers(data){
+    //   const contributer = data.contributer
+    //   parent.appendChild(elem);
+  
+  
+    // }
+  
+    fetch(
+      `https://api.github.com/repos/HackYourFuture/${selected}/contributors`,
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+
+          renderContributor(data, contriUl)
+  
+      })
+      .catch(err => console.error(err));
+  
+  
+  
+  
+  })
+  
+  
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
 
-const root = document.querySelector('#root');
-const header = document.createElement('div');
-header.setAttribute('class', 'header');
-header.innerText = 'HYF Repositories';
-const dropdownL = document.createElement('select');
-header.appendChild(dropdownL);
-dropdownL.setAttribute('class', 'dropdownL');
-
-document.body.insertBefore(header, root);
-
-
-const contributerBox =document.createElement('div');
-contributerBox.setAttribute('class', 'contributerBox');
-root.appendChild(contributerBox);
 
 
 
@@ -160,29 +261,9 @@ root.appendChild(contributerBox);
 
 
 
-dropdownL.addEventListener('change', function(){
-  const ul = document.querySelector('.ul');
-  ul.setAttribute('class', 'ul_onclick');
-  contributerBox.style.display='block';
 
-  const li = document.querySelector('.li')
 
-  fetchJSON(url, (err, repos) => {
-    
-    if (err) {
-      createAndAppend('div', ul, {
-        text: err.message,
-        class: 'alert-error',
-      });
-      return;
-    }
-   
 
-    const contriUl = createAndAppend('ul', contributerBox);
-    contriUl.setAttribute('class', 'contriUl')
 
-    renderContributor(repos[i], contriUl);
-    
- 
-  })
-})
+
+
